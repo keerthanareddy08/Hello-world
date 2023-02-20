4f387de61fdb92791367a079ed563b5b23881588
keerthanareddy08 <125845839+keerthanareddy08@users.noreply.github.com>
2023-02-20 11:55:31 +0530

    Add files via upload

---
+++ Friend.java
@@ -0,0 +1,9 @@
+public class Friend{
+
+// main method
+public static void main(String[] args){
+	System.out.println("Dear friend..!");
+	System.out.println("bakki");
+	System.out.println("How are you doing..");
+}
+}
---
+++ conversation.java
@@ -0,0 +1,11 @@
+public class conversation{
+
+// main method
+public static void main(String[] args){
+	System.out.println("hey are you there...");
+	System.out.println("yes");
+	System.out.println("tell me what you know about the java");
+	System.out.println("fine i dont have any knowledge on java");
+	System.out.println("alright i am gonna teach you now about the basics");
+}
+}
